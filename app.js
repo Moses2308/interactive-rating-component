b59ce66c9108a0c50b722a9a652440c1daafb20c
Moses <moses2308ventura@gmail.com>
2023-07-01 22:03:42 -0700

    added app.js and prevented form submission

---
+++ app.js
@@ -0,0 +1,5 @@
+let form = document.querySelector("#rating-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+// TODO : prevent form submission, get data from form, make new card using data
