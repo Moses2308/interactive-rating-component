221a20be9c661526e10b7f4267688a511f4c310c
Moses <moses2308ventura@gmail.com>
2023-07-04 19:24:39 -0700

    created HTML for Submitted State

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ const submittedForm = document.querySelector("#submitted-form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   openForm.style.display = "none";
+  submittedForm.style.display = "block";
 });
 // TODO : prevent form submission, get data from form, make new card using data
