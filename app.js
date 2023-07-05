6f2ed4f1776e73c0932c2f0094133093e11861bc
Moses <moses2308ventura@gmail.com>
2023-07-04 19:06:24 -0700

    adjusted HTML, Created JS to make form Content Hide

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-let form = document.querySelector("#rating-form");
+const form = document.querySelector("#rating-form");
+const openForm = document.querySelector("#open-form");
+const submittedForm = document.querySelector("#submitted-form");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  openForm.style.display = "none";
 });
 // TODO : prevent form submission, get data from form, make new card using data
