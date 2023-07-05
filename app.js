38cd3ee69fa5aa586c93c0b6521b5c002a77edba
Moses <moses2308ventura@gmail.com>
2023-07-04 20:38:02 -0700

    created logic for getting and displaying rating

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const form = document.querySelector("#rating-form");
 const openForm = document.querySelector("#open-form");
 const submittedForm = document.querySelector("#submitted-form");
+const inputButtons = document.querySelectorAll("input[type=radio]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  inputButtons.forEach((element) => {
+    if (element.checked === true) {
+      const ratingValue = element.dataset.rating;
+      submittedForm.querySelector(".customer-rating").textContent = ratingValue;
+    }
+  });
+
   openForm.style.display = "none";
   submittedForm.style.display = "grid";
 });
-// TODO : get data from form, make new card using data
